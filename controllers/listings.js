811e6dcb3338888fd9da35a131ed1d9680e08ffd
Sachin K Komar <sachinkkv08@gmail.com>
2025-11-18 15:27:10 +0530

    Fix controller export and routing error

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,39 +1,106 @@
+const Listing = require("../models/listing");
+const forwardGeocode = require("../utils/geocode");
+
+// SHOW ALL LISTINGS
+module.exports.index = async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+};
+
+
+// RENDER NEW LISTING FORM
+module.exports.renderNewForm = (req, res) => {
+  res.render("listings/new.ejs");
+};
+
+// SHOW SINGLE LISTING
+module.exports.showListing = async (req, res) => {
+  const { id } = req.params;
+
+  const listing = await Listing.findById(id)
+    .populate({
+      path: "reviews",
+      populate: { path: "author" }
+    })
+    .populate("owner");
+
+  if (!listing) {
+    req.flash("error", "Listing you requested does not exist");
+    return res.redirect("/listings");
+  }
+
+  res.render("listings/show.ejs", { listing });
+};
+
+// CREATE LISTING
 module.exports.createListing = async (req, res, next) => {
   try {
     const newListing = new Listing(req.body.listing);
 
-    // Geocode using OpenStreetMap
-    console.log("📍 Searching coordinates for:", req.body.listing.location);
+    // 1. Geocode
     const geometry = await forwardGeocode(req.body.listing.location);
-
     newListing.geometry = geometry;
 
-    // Image upload
+    // 2. Image upload
     if (req.file) {
       newListing.image = {
         url: req.file.path,
         filename: req.file.filename
       };
-    } else {
-      newListing.image = {
-        url: "https://res.cloudinary.com/demo/image/upload/v1699549950/default_image.jpg",
-        filename: "default_image"
-      };
     }
 
-    // Owner
+    // 3. Owner
     newListing.owner = req.user._id;
 
-    // Save listing
+    // 4. Save
     await newListing.save();
-    console.log("Listing saved successfully:", newListing);
 
     req.flash("success", "New Listing Created!");
     res.redirect(`/listings/${newListing._id}`);
-
   } catch (err) {
-    console.error("Error creating listing:", err);
-    req.flash("error", `Failed to create listing. Reason: ${err.message}`);
+    req.flash("error", err.message);
     res.redirect("/listings");
   }
+};
+
+// RENDER EDIT FORM
+module.exports.renderEditForm = async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+
+  if (!listing) {
+    req.flash("error", "Listing you requested does not exist!");
+    return res.redirect("/listings");
+  }
+
+  res.render("listings/edit.ejs", { listing });
+};
+
+
+// UPDATE LISTING
+module.exports.updateListing = async (req, res) => {
+  let { id } = req.params;
+
+  let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+
+  if (req.file) {
+    listing.image = {
+      url: req.file.path,
+      filename: req.file.filename
+    };
+    await listing.save();
+  }
+
+  req.flash("success", "Listing Updated!");
+  res.redirect(`/listings/${id}`);
+};
+
+// DELETE LISTING
+module.exports.destroyListing = async (req, res) => {
+  let { id } = req.params;
+
+  await Listing.findByIdAndDelete(id);
+
+  req.flash("success", "Listing Deleted");
+  res.redirect("/listings");
 };
